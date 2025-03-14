943370e07452d47bcc78a4ff5243d07404c23b09
Odin <148846437+SlightRecoil@users.noreply.github.com>
2025-03-14 11:24:08 +0100

    added code for opening file, started coding for reading file

--- src/bmp_reader.c
+++ src/bmp_reader.c
@@ -3,6 +3,43 @@
 void ctoy_begin() // called at the beginning of the program
 {
    printf("Hello, World!\n");
+   const char* fname = "data/tetris.bmp"; // or tmpnam(NULL);
+   int is_ok = EXIT_FAILURE;
+
+   FILE* fp = fopen(fname, "w+");
+   if (!fp)
+   {
+       perror("File opening failed");
+       return;
+   }
+
+   // find size of file
+   if(fseek(fp,0L,SEEK_END)!=0)
+   {
+      perror("File opening failed");
+      fclose(fp);
+      return;
+   }
+   size_t size = ftell(fp);
+   printf("%u\n",size);
+   rewind(fp);
+
+   // define target
+   uint8_t * ptr = malloc(size);
+
+   // read file
+   fread(ptr, sizeof uint8_t, size, fp);
+   // fehlerbehandlung malloc, fread
+   // in einer funktion: readFileToMemory(const char * filename)
+   // returnwert: struct{ptr, size} ; typedef MEMPTR
+   // uint8_t readBYTE( uint8_t *data, size_t offset)
+   
+
+
+   //fread(b, sizeof b[0], SIZE, fp); 
+
+   fclose(fp);
+
 }
 
 void ctoy_main_loop() // called at every update of the main loop
